f8311ca65adedbb5d059a99d97b3417e35397ea0
An Doan <adoan@aithinktank.com>
2021-05-02 11:00:59 -0700

    Implement GEventListeners.

--- Qt/ProgrammingAssignment1/main.cpp
+++ Qt/ProgrammingAssignment1/main.cpp
@@ -8,65 +8,53 @@
 
 using namespace std;
 
+volatile bool isDrawing = true;
 const int TRIANGLE_POINTS = 3;
 
-GPoint ClickPoint()
-{
-    while(true)
-    {
+GPoint ClickPoint() {
+    while(true) {
         GMouseEvent e = waitForEvent(MOUSE_EVENT);
-        if(e.getEventType() == MOUSE_CLICKED)
-        {
+        if(e.getEventType() == MOUSE_CLICKED) {
             return GPoint(e.getX(), e.getY());
         }
     }
 }
 
-GPoint GetMidpoint(const GPoint& pA, const GPoint& pB)
-{
+GPoint GetMidpoint(const GPoint& pA, const GPoint& pB) {
     int mid_x = (pA.x + pB.x)/2;
     int mid_y = (pA.y + pB.y)/2;
     return GPoint(mid_x, mid_y);
 }
 
-void DrawAndUpdatePoint(GWindow& gw, const vector<GPoint>& points, GPoint& prev)
-{
+void DrawAndUpdatePoint(GWindow& gw, const vector<GPoint>& points, GPoint& prev) {
     int draw_idx = randomInteger(0, TRIANGLE_POINTS-1);
-    //gw.pause(100);
     prev = GetMidpoint(points[draw_idx], prev);
     gw.fillOval(prev.x, prev.y, 3, 3);
 }
 
-void ChaosGame(GWindow& gw, const vector<GPoint>& points)
-{
-    GTimer timer(1);
-    timer.start();
+void StopDrawing(GEvent e) {
+    if(e.getEventType() == MOUSE_CLICKED) isDrawing = false;
+}
+
+void ChaosGame(GWindow& gw, const vector<GPoint>& points) {
     gw.setColor("black");
     GPoint prev = points[randomInteger(0, TRIANGLE_POINTS-1)];
-    while(true)
-    {
-        GEvent e = waitForEvent(MOUSE_EVENT+TIMER_EVENT);
-        if(e.getEventType() == MOUSE_CLICKED)
-        {
-            return;
-        }
+    gw.setMouseListener(StopDrawing);
+    while(isDrawing) {
         DrawAndUpdatePoint(gw, points, prev);
         gw.repaint();
     }
 }
 
-int main()
-{
+int main() {
     GWindow gw;
     vector<GPoint> points(TRIANGLE_POINTS);
-
     cout << "Click three points to create a triangle." << endl;
-    for(int i = 0; i < TRIANGLE_POINTS; i++)
-    {
+    for(int i = 0; i < TRIANGLE_POINTS; i++) {
         points[i] = ClickPoint();
     }
-    cout << "Starting Chaos Game. Click to end Chaos Game." << endl;
 
+    cout << "Starting Chaos Game. Click to end Chaos Game." << endl;
     ChaosGame(gw, points);
     return 0;
 }
